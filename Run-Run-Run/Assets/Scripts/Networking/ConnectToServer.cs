f1063c1ccbf51246783eee13666026e0e91b9b52
MalharDevasthali <malhardevasthali007@gmail.com>
2022-06-26 16:47:47 +0530

    Loading Scene and Lobby Scene Created

--- Run-Run-Run/Assets/Scripts/Networking/ConnectToServer.cs
+++ Run-Run-Run/Assets/Scripts/Networking/ConnectToServer.cs
@@ -2,19 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.SceneManagement;
 
-public class ConnectToServer : MonoBehaviour
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
 
 {
     // Start is called before the first frame update
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnConnectedToMaster()
     {
-        
+        PhotonNetwork.JoinLobby();
+
     }
+    public override void OnJoinedLobby()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+  
 }
